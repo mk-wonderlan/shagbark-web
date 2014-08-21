3ea192d0bb94640bcd90b9181b761ad218834136
Otto Remse <otto.remse@gmail.com>
2014-08-22 01:54:05 +0200

    first rev

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'js')));
+app.get('/',function(req,res)
+{
+  res.render('index.html');
+})
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
